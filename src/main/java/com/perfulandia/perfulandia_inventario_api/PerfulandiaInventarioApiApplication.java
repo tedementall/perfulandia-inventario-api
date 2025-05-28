8736db60333fefff8527aa5ce682b8844e816d46
Andru Alarcon <idazhdz@gmail.com>
2025-05-27 22:44:32 -0400

    Commit inicial del proyecto

---
+++ src/main/java/com/perfulandia/perfulandia_inventario_api/PerfulandiaInventarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_inventario_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaInventarioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaInventarioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/perfulandia_inventario_api/PerfulandiaInventarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_inventario_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaInventarioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
